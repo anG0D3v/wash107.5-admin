f9f35321cccec2f6e64340ff840785f5b9b749f7
anG0D3v <angod3v@gmail.com>
2023-11-02 00:18:57 +0800

    Added showing of users

--- src/components/button/CustomButton.tsx
+++ src/components/button/CustomButton.tsx
@@ -25,10 +25,13 @@ const CustomButton: React.FC<BtnProps> = ({
     onClick={onClick}
     {...props}
     className={clsx(
+      'text-white font-semibold py-2 px-4 rounded-lg shadow-md transition duration-300 ease-in-out transform hover:scale-105',
       type === 'primary'
-        ? 'bg-blue-500 hover:bg-blue-700 cursor-pointer text-white font-semibold py-2 px-4 rounded-lg shadow-md transition duration-300 ease-in-out transform hover:scale-105'
-        : 'bg-pink-700 hover:bg-pink-900 text-white font-semibold py-2 cursor-pointer px-4 rounded-lg shadow-md transition duration-300 ease-in-out transform hover:scale-105',
-      disabled ? 'bg-gray-400 hover:bg-gray-400' : '',
+        ? 'bg-blue-500 hover:bg-blue-700 cursor-pointer '
+        : disabled
+        ? 'bg-gray-400 hover:bg-gray-400'
+        : 'bg-pink-700 hover:bg-pink-900',
+
       addedClass,
     )}
   >
